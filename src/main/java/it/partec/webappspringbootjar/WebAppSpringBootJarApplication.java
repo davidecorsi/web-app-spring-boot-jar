45ce987f74c6b523d9c81f3f74a1fbc59bceb28f
Davide Corsi <davide.corsi@par-tec.it>
2021-08-18 11:43:39 +0200

    first commit

---
+++ src/main/java/it/partec/webappspringbootjar/WebAppSpringBootJarApplication.java
@@ -0,0 +1,13 @@
+package it.partec.webappspringbootjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSpringBootJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSpringBootJarApplication.class, args);
+	}
+
+}
